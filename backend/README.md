9dc7fb3d9e12332d3b5545524eb33dfcb473db43
KostyaIliouk <kostya.iliouk@hotmail.com>
2020-03-12 15:49:58 -0400

    small edit to the Readme and added todo tag for switching to https

--- backend/README.md
+++ backend/README.md
@@ -1,9 +1,9 @@
 # Backend
 
 ## Instructions
-
+ - run `npm install` to install all needed dependencies
  - run `node app.js` or `nodemon app.js`
-- //TODO: containerize
+ - //TODO: containerize
 
 ## Credits
 
--- backend/app.js
+++ backend/app.js
@@ -10,10 +10,12 @@ app.use(function(req, res, next){
     next();
 });
 
+// setup http connection
+// TODO: switch to https once hosting figured out
 const http = require('http');
-
-http.createServer(app).listen(3000, function(err){
+const PORT = 3000;
+http.createServer(app).listen(PORT, function(err){
     if (err) console.log(err);
-    else console.log(`HTTP server on http://localhost:3000`);
+    else console.log(`HTTP server on http://localhost:${PORT}`);
 });
 
