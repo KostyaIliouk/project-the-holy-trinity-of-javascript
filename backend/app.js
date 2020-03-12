88e295b1cc012b78fb77060f61509d6c58d096ca
KostyaIliouk <kostya.iliouk@hotmail.com>
2020-03-12 02:18:08 -0400

    basic http, switch to https later

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,19 @@
 const express = require('express');
-const https = require('https');
-const fs = require("fs");
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.json());
 
-const config = {
-    // if they fail then server doesnt start anyway so may as well be sync
-    key: fs.readFileSync('./secret/server.key'),
-    cert: fs.readFileSync('./secret/server.crt')
-};
+app.use(function(req, res, next){
+    console.log("HTTP request", req.method, req.url, req.body);
+    next();
+});
 
-https.createServer(app, config).listen(3000, function(err){
+const http = require('http');
+
+http.createServer(app).listen(3000, function(err){
     if (err) console.log(err);
-    else console.log(`HTTPS server on https://localhost:3000`);
+    else console.log(`HTTP server on http://localhost:3000`);
 });
 
