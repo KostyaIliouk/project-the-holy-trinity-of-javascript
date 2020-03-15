1b1aa5b6425d89855547d5ca991ad40069ee7507
KostyaIliouk <kostya.iliouk@hotmail.com>
2020-03-15 01:35:25 -0400

    restructing reddit api into backend server with newsapi

--- trendmap/backend/routing/routing.js
+++ trendmap/backend/routing/routing.js
@@ -3,10 +3,12 @@
 
 const router = require("express").Router();
 const newsapirouting = require("../newsapihandler/routing");
+const redditrouting = require("../redditapi/routing");
 
 // split the routing between different api sections
 
 // newsapi routing
 router.use('/newsapi', newsapirouting);
+router.use('/reddit', redditrouting);
 
 module.exports = router;
