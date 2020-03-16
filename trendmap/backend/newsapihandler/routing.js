27718b1f15c771bcd53f7fad3003db958f58bd05
Mohammad <mohammad.m@live.ca>
2020-03-15 20:12:11 -0400

    minor semantic fixes

--- trendmap/backend/newsapihandler/routing.js
+++ trendmap/backend/newsapihandler/routing.js
@@ -7,6 +7,6 @@ const validator = require("../middleware/validator");
 const consolidator = require("../middleware/consolidator");
 
 // get headlines of given country
-router.get('/getHeadlines', validator.newsapiGetHeadlines, newsapi.getHeadlines, consolidator.newsapiGetHeadlines);
+router.get('/getHeadlines', validator.validateCountry, newsapi.getHeadlines, consolidator.newsapiGetHeadlines);
 
 module.exports = router;
