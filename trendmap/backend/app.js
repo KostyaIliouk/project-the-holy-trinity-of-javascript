11e10cf9e35d5931de98e0e324cfc20a361278f3
Hessamshar <hessamoddin.sharifpour@gmail.com>
2020-03-28 22:19:25 -0700

    Replace REST API with Socket.io and imporve frontend

--- trendmap/backend/app.js
+++ trendmap/backend/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const routing = require('./routing/routing');
 const redis = require('./redishandler/redis');
 
 const app = express();
+
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const socketio = require('./socketio/socketio')(io)
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -11,20 +14,16 @@ app.use(bodyParser.urlencoded({extended: true}));
 // just to log requests comming in to console - should remove this later or
 // switch to an actual logger
 app.use(function(req, res, next){
-    console.log("HTTP request", req.method, req.url, req.body);
+    console.log('HTTP request', req.method, req.url, req.body);
     next();
 });
 
 redis.setupWorkers();
 
-// handle all possible routing
-app.use('/', routing);
-
 // setup http connection
 // TODO: switch to https once hosting figured out
-const http = require('http');
 const PORT = 5000;
-http.createServer(app).listen(PORT, function(err){
+http.listen(PORT, function(err){
     if (err) console.log(err);
     else console.log(`HTTP server on http://localhost:${PORT}`);
 });
